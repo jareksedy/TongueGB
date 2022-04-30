d686d72389581ab28904b07d89d851993b862f8f
Jarek Šedý <jareksedy@icloud.com>
2022-04-30 11:53:07 +0600

    Added labels, outlets & action stubs.

--- ЯзыкЪ/Scenes/LoginScene/LoginSceneViewController.swift
+++ ЯзыкЪ/Scenes/LoginScene/LoginSceneViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 // MARK: - Protocol
 protocol LoginSceneViewDelegate: NSObjectProtocol {
@@ -20,13 +21,33 @@ extension LoginSceneViewController: LoginSceneViewDelegate {
 class LoginSceneViewController: UIViewController {
     lazy var presenter = LoginScenePresenter()
     
+    // MARK: - Properties
+    let greetingLabelText = "Добро пожаловать ✌️\nв приложение «ЯзыкЪ» 👅"
+    let greetingSubLabelText = "Удобный инструмент для изучения иностранных слов. Войдите в приложение со своим Apple ID"
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+    let appVersionLabelText = "Версия"
+    let loginButtonTitle = "Войти с Apple ID"
+    
+    let appleIDProvider = ASAuthorizationAppleIDProvider()
+    
     // MARK: - Methods
     private func setupUI() {
+        greetingLabel.text = greetingLabelText
+        greetingSubLabel.text = greetingSubLabelText
+        appVersionLabel.text = "\(appVersionLabelText) \(appVersion ?? "0.0.0")"
+        loginButton.setTitle(loginButtonTitle, for: .normal)
     }
 
     // MARK: - Outlets
+    @IBOutlet weak var logoImageView: UIImageView!
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var greetingSubLabel: UILabel!
+    @IBOutlet weak var appVersionLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
     
     // MARK: - Actions
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    }
     
     // MARK: - Selectors
     
