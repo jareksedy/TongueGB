5dbca0c7e2d78c4c1317670c0db19c631a591455
Jarek Šedý <jareksedy@icloud.com>
2022-05-02 09:40:47 +0600

    Added subclass for UITabBarController to hide back button.

---
+++ ЯзыкЪ/UIComponents/YazykTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  YazykTabBarController.swift
+//  ЯзыкЪ
+//
+//  Created by Ярослав on 02.05.2022.
+//
+
+import UIKit
+
+class YazykTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.hidesBackButton = true
+    }
+}
