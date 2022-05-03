a3f2c7114d26325e8e9f1e9a1c67dbe8d2432f4d
Jarek <jareksedy@icloud.com>
2022-05-03 15:54:21 +0600

    Correctly displaying word labels.

--- ЯзыкЪ/UIComponents/CardView.swift
+++ ЯзыкЪ/UIComponents/CardView.swift
@@ -9,27 +9,44 @@ import UIKit
 
 // -- remove this later <start> --
 struct Card {
-     let word: String
-     let translation: String
-     let description: String?
-     let category: Category
-     let userEmail: String
- }
+    let word: String
+    let translation: String
+    let description: String?
+    let category: Category
+    let userEmail: String
+}
 
 struct Category {
-     let categoryKey: String
-     let categoryColor: String?
-     let categoryImage: String?
- }
+    let categoryKey: String
+    let categoryColor: String?
+    let categoryImage: String?
+}
 // -- remove his later <end> --
 
 class CardView: UIView {
-    var card: Card?
+    var card: Card? {
+        get {
+            return Card(word: wordLabel?.text ?? "",
+                        translation: translationLabel?.text ?? "",
+                        description: descriptionLabel?.text ?? "",
+                        category: Category(categoryKey: categoryLabel?.text ?? "",
+                                           categoryColor: nil, categoryImage: nil),
+                        userEmail: self.userEmail ?? "")
+        }
+        
+        set {
+            wordLabel?.text = newValue?.word
+            descriptionLabel?.text = newValue?.description
+            translationLabel?.text = newValue?.translation
+            categoryLabel?.text = newValue?.category.categoryKey
+        }
+    }
     
-    var wordLabel: UILabel?
-    var descriptionLabel: UILabel?
-    var categoryLabel: UILabel?
-    var translationLabel: UILabel?
+    private var wordLabel: UILabel?
+    private var descriptionLabel: UILabel?
+    private var categoryLabel: UILabel?
+    private var translationLabel: UILabel?
+    private var userEmail: String?
     
     override var intrinsicContentSize: CGSize {
         return CGSize(width: 300, height: 0)
@@ -40,7 +57,7 @@ class CardView: UIView {
         setupView()
         setupConstraints()
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupView()
@@ -53,7 +70,6 @@ class CardView: UIView {
         
         wordLabel = UILabel()
         wordLabel?.font = UIFont.preferredFont(forTextStyle: .title2)
-        wordLabel?.text = "Херъ"// card?.word
         addSubview(wordLabel!)
     }
     
