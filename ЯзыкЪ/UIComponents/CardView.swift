6af912fb534d0ef77ee88abf4573b936a057c8c7
Jarek <jareksedy@icloud.com>
2022-05-03 17:44:06 +0600

    Additional labels in CardView.

--- ЯзыкЪ/UIComponents/CardView.swift
+++ ЯзыкЪ/UIComponents/CardView.swift
@@ -42,6 +42,7 @@ class CardView: UIView {
         }
     }
     
+    private var speakButton: UIButton?
     private var wordLabel: UILabel?
     private var descriptionLabel: UILabel?
     private var categoryLabel: UILabel?
@@ -64,18 +65,48 @@ class CardView: UIView {
         setupConstraints()
     }
     
+    @objc func speakButtonTapped() {
+        print("i speak \(card?.word ?? "")")
+    }
+    
     func setupView() {
         self.backgroundColor = UIColor.random
-        self.layer.cornerRadius = 18.0
+        self.layer.cornerRadius = 14.0
         
         wordLabel = UILabel()
         wordLabel?.font = UIFont.preferredFont(forTextStyle: .title2)
         addSubview(wordLabel!)
+        
+        descriptionLabel = UILabel()
+        descriptionLabel?.font = UIFont.preferredFont(forTextStyle: .footnote)
+        addSubview(descriptionLabel!)
+        
+        categoryLabel = UILabel()
+        categoryLabel?.font = UIFont.preferredFont(forTextStyle: .caption2)
+        addSubview(categoryLabel!)
+        
+        speakButton = UIButton()
+        speakButton?.setImage(UIImage(systemName: "speaker.wave.1")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        speakButton?.tintColor = .label
+        speakButton?.addTarget(self, action: #selector(speakButtonTapped), for: .touchUpInside)
+        addSubview(speakButton!)
     }
     
     func setupConstraints() {
         wordLabel?.translatesAutoresizingMaskIntoConstraints = false
         wordLabel?.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        wordLabel?.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        wordLabel?.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -10).isActive = true
+        
+        descriptionLabel?.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel?.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        descriptionLabel?.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 25).isActive = true
+        
+        categoryLabel?.translatesAutoresizingMaskIntoConstraints = false
+        categoryLabel?.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        categoryLabel?.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -50).isActive = true
+        
+        speakButton?.translatesAutoresizingMaskIntoConstraints = false
+        speakButton?.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        speakButton?.topAnchor.constraint(equalTo: self.topAnchor, constant: 50).isActive = true
     }
 }
