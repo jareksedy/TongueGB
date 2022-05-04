499b64bd2dc2fbef6b6f5b82297c2dd68aa70532
Denis <recmanus@gmail.com>
2022-05-04 10:14:21 +0300

    Create models and API
    Добавлены модели и подготовлен протокол и класс под FirebaseAPI.

---
+++ ЯзыкЪ/Models/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  ЯзыкЪ
+//
+//  Created by Denis Kazarin on 04.05.2022.
+//
+
+import Foundation
+
+struct Card {
+    let word: String
+    let translation: String
+    let description: String?
+    let category: CardsCategory
+    let userEmail: String
+}
---
+++ ЯзыкЪ/Models/CardsCategory.swift
@@ -0,0 +1,14 @@
+//
+//  CardsCategory.swift
+//  ЯзыкЪ
+//
+//  Created by Denis Kazarin on 04.05.2022.
+//
+
+import Foundation
+
+struct CardsCategory {
+    let categoryKey: String
+    let categoryColor: String?
+    let categoryImage: String?
+}
