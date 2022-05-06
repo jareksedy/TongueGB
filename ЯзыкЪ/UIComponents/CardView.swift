f5652631cdb7d3b170570af1f709a28dd78624be
Jarek Šedý <jareksedy@icloud.com>
2022-05-07 00:05:00 +0600

    Fine-tune speak button edge insets.

--- ЯзыкЪ/UIComponents/CardView.swift
+++ ЯзыкЪ/UIComponents/CardView.swift
@@ -166,7 +166,7 @@ class CardView: UIControl {
         speakButton?.setImage(UIImage(systemName: "speaker.wave.1")?.withRenderingMode(.alwaysTemplate), for: .normal)
         speakButton?.tintColor = .label
         speakButton?.addTarget(self, action: #selector(speakButtonTapped), for: .touchUpInside)
-        speakButton?.contentEdgeInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        speakButton?.contentEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         speakButton?.adjustsImageWhenHighlighted = false
         frontView.addSubview(speakButton!)
         
@@ -232,7 +232,7 @@ class CardView: UIControl {
         
         speakButton?.translatesAutoresizingMaskIntoConstraints = false
         speakButton?.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        speakButton?.topAnchor.constraint(equalTo: self.topAnchor, constant: 30).isActive = true
+        speakButton?.topAnchor.constraint(equalTo: self.topAnchor, constant: 40).isActive = true
     }
     
     private func setupBackViewConstraints() {
