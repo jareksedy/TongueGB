3a5ca0d72109ea0c64787e41596ab429f30f6459
Jarek Šedý <jareksedy@icloud.com>
2022-05-14 11:44:11 +0600

    Refactor Profile Scene.

--- ЯзыкЪ/App/Flows/ProfileScene/ProfileSceneViewController.swift
+++ ЯзыкЪ/App/Flows/ProfileScene/ProfileSceneViewController.swift
@@ -11,74 +11,10 @@ import UIKit
 protocol ProfileSceneViewDelegate: NSObjectProtocol {
 }
 
-// MARK: - Implementation
-extension ProfileSceneViewController: ProfileSceneViewDelegate {
-}
-
-// MARK: - Additional extensions
 // MARK: - View controller
 class ProfileSceneViewController: UIViewController {
     lazy var presenter = ProfileScenePresenter()
     
-    // MARK: - Services
-    let greetingGenerator = GreetingGenerator()
-    
-    // MARK: - Properties
-    var randomGreeting: Greeting?
-    let greetingLabelText = "Теперь вы знаете как поприветствовать кого-нибудь"
-    let captionCardsNumberLabelText = "Количество карточек"
-    let captionCategoriesNumberLabelText = "Количество категорий"
-    let captionRegistrationDateLabelText = "Дата регистрации"
-    let aboutAppHeadingLabelText = "О приложении"
-    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-    
-    // MARK: - Methods
-    private func setupUI() {
-        randomGreeting = greetingGenerator.randomGreeting()
-        
-        transcriptionLabel.text = randomGreeting?.transcription
-        transcriptionLabel.font = UIFont.preferredFont(forTextStyle: .footnote)
-        transcriptionLabel.alpha = 0.5
-        
-        greetingLabel.text = "\(greetingLabelText) \(randomGreeting?.language ?? "")."
-        greetingLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        
-        captionCardsNumberLabel.text = captionCardsNumberLabelText
-        captionCardsNumberLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        captionCardsNumberLabel.textColor = .secondaryLabel
-        
-        cardsNumberLabel.text = "48"
-        cardsNumberLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        cardsNumberLabel.textColor = .secondaryLabel
-        
-        captionCategoriesNumberLabel.text = captionCategoriesNumberLabelText
-        captionCategoriesNumberLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        captionCategoriesNumberLabel.textColor = .secondaryLabel
-        
-        categoriesNumberLabel.text = "14"
-        categoriesNumberLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        categoriesNumberLabel.textColor = .secondaryLabel
-        
-        captionRegistrationDateLabel.text = captionRegistrationDateLabelText
-        captionRegistrationDateLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        captionRegistrationDateLabel.textColor = .secondaryLabel
-        
-        registrationDateLabel.text = "9 мая 2022"
-        registrationDateLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        registrationDateLabel.textColor = .secondaryLabel
-        
-        aboutAppHeadingLabel.text = aboutAppHeadingLabelText
-        aboutAppHeadingLabel.font = UIFont.preferredFont(forTextStyle: .title3)
-        
-        aboutAppTextLabel.text = "Разработано Like-Minded People. Денис Казарин, Кирилл Сухов, Ярослав Седышев. Версия \(appVersion ?? "0.0.0"). Распространяется по лицензии MIT."
-        aboutAppTextLabel.font = UIFont.preferredFont(forTextStyle: .subheadline)
-        aboutAppTextLabel.textColor = .secondaryLabel
-    }
-    
-    private func setupNavigationOptions() {
-        self.tabBarController?.title = "\(randomGreeting?.hello ?? "Привет"), Ярослав!"
-    }
-
     // MARK: - Outlets
     @IBOutlet weak var transcriptionLabel: UILabel!
     @IBOutlet weak var greetingLabel: UILabel!
@@ -91,9 +27,13 @@ class ProfileSceneViewController: UIViewController {
     @IBOutlet weak var aboutAppHeadingLabel: UILabel!
     @IBOutlet weak var aboutAppTextLabel: UILabel!
     
-    // MARK: - Actions
+    // MARK: - Services
+    let greetingGenerator = GreetingGenerator()
     
-    // MARK: - Selectors
+    // MARK: - Properties
+    var randomGreeting: Greeting!
+    let greetingLabelText = "Теперь вы знаете как поприветствовать кого-нибудь"
+    let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -106,4 +46,26 @@ class ProfileSceneViewController: UIViewController {
         super.viewWillAppear(animated)
         setupNavigationOptions()
     }
+    
+    // MARK: - Private methods
+    private func setupUI() {
+        randomGreeting = greetingGenerator.randomGreeting()
+        
+        transcriptionLabel.text = randomGreeting?.transcription
+        greetingLabel.text = "\(greetingLabelText) \(randomGreeting?.language ?? "")."
+        
+        cardsNumberLabel.text = "36"
+        categoriesNumberLabel.text = "4"
+        registrationDateLabel.text = "9 мая 2022"
+        
+        aboutAppTextLabel.text = "Разработано Like-Minded People. Версия \(appVersion ?? "0.0.0"). Распространяется по лицензии MIT."
+    }
+    
+    private func setupNavigationOptions() {
+        self.tabBarController?.title = "\(randomGreeting?.hello ?? "Привет"), Ярослав!"
+    }
+}
+
+// MARK: - Implementation
+extension ProfileSceneViewController: ProfileSceneViewDelegate {
 }
