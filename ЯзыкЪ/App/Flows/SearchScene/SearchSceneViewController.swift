30f19c6f2e6bd9db75d4ebafa1b4315e962617a7
Jarek Šedý <jareksedy@icloud.com>
2022-05-14 11:21:55 +0600

    Refactor Search Scene.

--- ЯзыкЪ/App/Flows/SearchScene/SearchSceneViewController.swift
+++ ЯзыкЪ/App/Flows/SearchScene/SearchSceneViewController.swift
@@ -11,38 +11,27 @@ import UIKit
 protocol SearchSceneViewDelegate: NSObjectProtocol {
 }
 
-// MARK: - Implementation
-extension SearchSceneViewController: SearchSceneViewDelegate {
-}
-
-// MARK: - Additional extensions
 // MARK: - View controller
 class SearchSceneViewController: UIViewController {
     lazy var presenter = SearchScenePresenter()
-    
-    // MARK: - Methods
-    private func setupUI() {
-    }
-    
-    private func setupNavigationOptions() {
-        self.tabBarController?.title = "Поиск"
-    }
-
-    // MARK: - Outlets
-    
-    // MARK: - Actions
-    
-    // MARK: - Selectors
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.viewDelegate = self
-        setupUI()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         setupNavigationOptions()
     }
+    
+    // MARK: - Private methods
+    private func setupNavigationOptions() {
+        self.tabBarController?.title = "Поиск и категории"
+    }
+}
+
+// MARK: - Implementation
+extension SearchSceneViewController: SearchSceneViewDelegate {
 }
