06b483e34a070478e1e4086cd61333e6bfea9d10
Jarek Šedý <jareksedy@icloud.com>
2022-05-14 11:04:19 +0600

    Refactor MainTabBarController.

--- ЯзыкЪ/App/Flows/MainTabBarController/MainTabBarController.swift
+++ ЯзыкЪ/App/Flows/MainTabBarController/MainTabBarController.swift
@@ -8,38 +8,34 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-    // MARK: - Overrides
-    override var selectedViewController: UIViewController? {
-        didSet {
-            tabChangedTo(selectedIndex: selectedIndex)
-        }
-    }
-    
-    override var selectedIndex: Int {
-        didSet {
-            tabChangedTo(selectedIndex: selectedIndex)
-        }
-    }
-
-     func tabChangedTo(selectedIndex: Int) {
-         if selectedIndex != 0 {
-             self.navigationItem.rightBarButtonItem = nil
-         } else {
-             self.navigationItem.rightBarButtonItem = categoriesBarButtonItem
-         }
-     }
     
     // MARK: - Outlets
     @IBOutlet var categoriesBarButtonItem: UIBarButtonItem!
     
-    // MARK: - Actions
-    @IBAction func categoriesBarButtonItemTapped(_ sender: Any) {
+    // MARK: - Overrides
+    override var selectedViewController: UIViewController? {
+        didSet { tabChangedTo(selectedIndex: selectedIndex) }
+    }
+    
+    override var selectedIndex: Int {
+        didSet { tabChangedTo(selectedIndex: selectedIndex) }
     }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.navigationItem.hidesBackButton = true
         self.navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    // MARK: - Actions
+    @IBAction func categoriesBarButtonItemTapped(_ sender: Any) {
+    }
+    
+    // MARK: - Private methods
+     private func tabChangedTo(selectedIndex: Int) {
+         if selectedIndex != 0 { self.navigationItem.rightBarButtonItem = nil } else {
+             self.navigationItem.rightBarButtonItem = categoriesBarButtonItem }
+     }
 }
