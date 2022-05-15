ddd5f200d391948a7312dc602a7810c1bb030a94
Jarek Šedý <jareksedy@icloud.com>
2022-05-15 15:26:43 +0600

    Basic tableview setup.

--- ЯзыкЪ/App/Flows/SearchScene/SearchSceneViewController.swift
+++ ЯзыкЪ/App/Flows/SearchScene/SearchSceneViewController.swift
@@ -15,10 +15,21 @@ protocol SearchSceneViewDelegate: NSObjectProtocol {
 class SearchSceneViewController: UIViewController {
     lazy var presenter = SearchScenePresenter()
     
+    // MARK: - Outlets
+    @IBOutlet weak var categoriesTableView: UITableView!
+    
+    // MARK: - Services
+    let mockCategoriesProvider = MockCategoriesProvider()
+    
+    // MARK: - Properties
+    var categories: [CardsCategory]?
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchCategories()
         presenter.viewDelegate = self
+        categoriesTableView.dataSource = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -30,6 +41,24 @@ class SearchSceneViewController: UIViewController {
     private func setupNavigationOptions() {
         self.tabBarController?.title = "Поиск и категории"
     }
+    
+    private func fetchCategories() {
+        categories = mockCategoriesProvider.createMockCategories()
+    }
+}
+// MARK: - TableView
+extension SearchSceneViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int { 1 }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { categories?.count ?? 0 }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = categories?[indexPath.row].categoryKey
+        
+        return cell
+    }
+    
+    
 }
 
 // MARK: - Implementation
