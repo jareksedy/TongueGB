5d2b8b4f0ada8728b53bbbd851ba6016011ac531
Jarek <jareksedy@icloud.com>
2022-05-19 16:57:46 +0600

    Presenting AddCardScene as sheet.

---
+++ ЯзыкЪ/App/Flows/Elements/MainTabBarController.swift
@@ -0,0 +1,37 @@
+//
+//  MainTabBarController.swift
+//  ЯзыкЪ
+//
+//  Created by Ярослав on 19.05.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController, UITabBarControllerDelegate {
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController.children[0] is AddCardSceneViewController {
+            if let addCardSceneViewController = self.storyboard?.instantiateViewController(withIdentifier: "AddCardScene") {
+                
+                let navigationController = UINavigationController(rootViewController: addCardSceneViewController)
+                navigationController.modalPresentationStyle = .pageSheet
+                
+                if #available(iOS 15.0, *) {
+                    if let sheet = navigationController.sheetPresentationController {
+                        sheet.detents = [.medium()]
+                        sheet.preferredCornerRadius = 24.0
+                    }
+                }
+                
+                present(navigationController, animated: true)
+                return false
+            }
+        }
+        return true
+    }
+}
