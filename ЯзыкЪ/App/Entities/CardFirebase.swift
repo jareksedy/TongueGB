4cadc70189b728c684db70c6c1dad2e1f5ac3e9a
Denis <recmanus@gmail.com>
2022-05-26 13:57:52 +0300

    Changing Card Model
    Добавлен timeStamp в формате TimeInterval

--- ЯзыкЪ/App/Entities/CardFirebase.swift
+++ ЯзыкЪ/App/Entities/CardFirebase.swift
@@ -14,6 +14,7 @@ class CardFirebase {
     let transcription: String?
     let category: String
     let userEmail: String
+    var timeStamp = Date().timeIntervalSince1970
     let ref: DatabaseReference?
     
     init(word: String, translation: String, transcription: String?, category: String, userEmail: String) {
@@ -32,7 +33,8 @@ class CardFirebase {
             let translation = value["translation"] as? String,
             let transcription = value["transcription"] as? String,
             let category = value["category"] as? String,
-            let userEmail = value["user_email"] as? String else {
+            let userEmail = value["user_email"] as? String,
+            let timeStamp = value["time_stamp"] as? Double else {
             return nil
         }
         self.word = word
@@ -40,6 +42,7 @@ class CardFirebase {
         self.transcription = transcription
         self.category = category
         self.userEmail = userEmail
+        self.timeStamp = timeStamp
         self.ref = snapshot.ref
     }
     
@@ -49,7 +52,8 @@ class CardFirebase {
             "translation": translation as Any,
             "transcription": transcription as Any,
             "category": category as Any,
-            "user_email": userEmail.modifyEmailAddress() as Any
+            "user_email": userEmail.modifyEmailAddress() as Any,
+            "time_stamp": timeStamp as Any
         ] as [String: Any]
     }
 }
