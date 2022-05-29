79eba8650de71f2ee3c7f1631abc57a7aec42608
Jarek Šedý <jareksedy@icloud.com>
2022-05-29 11:00:14 +0600

    Added popup alert extension.

--- ЯзыкЪ/Core/Helpers/UIViewController.swift
+++ ЯзыкЪ/Core/Helpers/UIViewController.swift
@@ -17,4 +17,13 @@ extension UIViewController {
             completionHandler?()
         }
     }
+    
+    func popupAlert(title: String?, message: String?, actionTitles:[String?], actionStyle:[UIAlertAction.Style], actions:[((UIAlertAction) -> Void)?], vc: UIViewController) {
+            let alert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            for (index, title) in actionTitles.enumerated() {
+                 let action = UIAlertAction(title: title, style: actionStyle[index], handler: actions[index])
+                 alert.addAction(action)
+            }
+            vc.present(alert, animated: true, completion: nil)
+       }
 }
